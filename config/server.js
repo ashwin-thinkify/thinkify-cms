84d5484e2fe7d61a38dce4da7fd9c99bb1353cb2
ashwin-thinkify <ak@thinkify.io>
2021-12-17 16:30:05 +0530

    Updated the url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://thinkify-be.herokuapp.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6009630389cd24561895f3c624933cc4'),
